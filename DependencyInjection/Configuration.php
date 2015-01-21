19143c80f0cd481bcac0af9b7f479c5a3507f208
Vova Soroka <vsoroka@magecore.com>
2015-01-21 02:06:43 +0200

    BAP-7153: Component and bundle skeleton creation. Rename OroBlockBundle and OroBlockComponent to OroLayoutBundle and OroLayoutComponent

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Oro\Bundle\LayoutBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('oro_layout');
+
+        return $treeBuilder;
+    }
+}
