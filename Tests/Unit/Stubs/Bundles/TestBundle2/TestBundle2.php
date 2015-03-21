43ad3817b45a1aa7ce5bcf4822081cc48fdaea9e
Vova Soroka <vsoroka@magecore.com>
2015-03-21 03:50:22 +0200

    BAP-7687: Move theme related functionality from layout bundle to LayoutComponent

--- Tests/Unit/Stubs/Bundles/TestBundle2/TestBundle2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Oro\Bundle\LayoutBundle\Tests\Unit\Stubs\Bundles\TestBundle2;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class TestBundle2 extends Bundle
-{
-}
--- Tests/Unit/Stubs/Updates/layout_update2.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-/** @var Oro\Component\Layout\LayoutManipulatorInterface $layoutManipulator */
-/** @var Oro\Component\Layout\LayoutItemInterface $item */
-
-$layoutManipulator->add('header', 'root', 'header');
